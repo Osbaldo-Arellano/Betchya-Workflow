cdd6ad7e7144a1780cf08e4b810515177533963a
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2024-02-20 20:05:52 -0800

    added a new function

---
+++ lambdas/testnew/go.mod
@@ -0,0 +1,5 @@
+module github.com/betchya/lambdas/testnew
+
+go 1.21.4
+
+require github.com/aws/aws-lambda-go v1.46.0 // indirect
---
+++ lambdas/testnew/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var greeting string
+	sourceIP := request.RequestContext.Identity.SourceIP
+
+	if sourceIP == "" {
+		fmt.Println("Hello from the new function")
+
+		greeting = "Hello from the new function!!!\n"
+	} else {
+		greeting = fmt.Sprintf("He2adsdasdassccsdasssdasdllo, %s!\n", sourceIP)
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       greeting,
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
